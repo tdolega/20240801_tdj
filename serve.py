272535e323404a50d860286b2984d2b8543b2e54
tdolega <tymoteuszdolega@gmail.com>
2024-08-01 21:08:03 +0200

    add support for env variables

--- serve.py
+++ serve.py
@@ -4,15 +4,17 @@ from flask_limiter.util import get_remote_address
 from werkzeug.exceptions import HTTPException
 import logging
 import argparse
+import os
 
-RATE_LIMIT = "3 per minute"
-API_KEY = "rekrutacja2024"
+RATE_LIMIT = os.getenv("RATE_LIMIT", "3 per minute")
+API_KEY = os.getenv("API_KEY", "rekrutacja2024")
+LOG_FILE = os.getenv("LOG_FILE", "app.log")
 REQUIRED_FORMAT = {
     "num": (int, float),
     "text": str,
 }
 
-logging.basicConfig(filename="app.log", level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
 
 app = Flask(__name__)
 limiter = Limiter(get_remote_address, app=app)
